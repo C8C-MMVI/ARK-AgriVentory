78f96e8cf37ebb41de6bba02afe95057ba3f28e4
C8C-MMVI <raivenchristian.navor@lorma.edu>
2025-11-21 12:52:23 +0800

    Note added

--- react_projex/src/components/Header.jsx
+++ react_projex/src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header({onSidebarToggle}){
     return(
-        <header>
+        <header className="">
             <div>
                 <button onClick={onSidebarToggle}>Toggle</button>
             </div>
@@ -8,6 +8,18 @@ function Header({onSidebarToggle}){
 
             </div>
         </header>
+        /*
+        SVG files:
+        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        <svg className="w-5 h-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+            </svg>      
+        */ 
     )
 }
 
