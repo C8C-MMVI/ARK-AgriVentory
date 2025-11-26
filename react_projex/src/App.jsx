665775bf2448bfbfbeebca2edc716cf9d8b02a18
C8C-MMVI <raivenchristian.navor@lorma.edu>
2025-11-26 10:33:56 +0800

    Frontend check

--- react_projex/src/App.jsx
+++ react_projex/src/App.jsx
@@ -1,17 +1,29 @@
-import Sidebar from "./components/Sidebar/"
-import Header from  "./components/Header/"
+import Sidebar from "./components/Sidebar"
+import Header from "./components/Header"
 import { useState } from "react"
+import { Routes, Route} from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Users from "./pages/Users"
 
-const App = () =>{
-  const[sidebarToggle, setSidebarToggle] = useState(true)
+const App = () => {
+  const [sidebarToggle, setSidebarToggle] = useState(true);
 
   function toggleSidebar(){
     setSidebarToggle(!sidebarToggle)
   }
-  return(
-    <div>
-      <Sidebar status = {sidebarToggle}/>
-      <Header onSidebarToggle = {toggleSidebar} />
+
+  return (
+    <div className="flex h-screen bg-gray-50">
+    <Sidebar isOpen = {sidebarToggle} />
+      <div className="flex-1 flex flex-col">
+        <Header onSidebarToggle = {toggleSidebar} />
+        <main className="flex-1 bg-slate-200">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/products" element={<Users />} />
+          </Routes>
+        </main>
+      </div>
     </div>
   )
 }
--- react_projex/src/components/Sidebar.jsx
+++ react_projex/src/components/Sidebar.jsx
@@ -1,23 +1,40 @@
-function Sidebar({status}){
+import { use } from "react"
+import {Link, useLocation} from "react-router-dom"
+
+function Sidebar({isOpen}){
+    const location = useLocation()  
     const menuItems = [
         {icon: "🏠", text: "Home", link:"/" },
         {icon: "👥", text: "User Dashboard", link:"/" },
-        {icon: "⚙️", text: "Settings", link:"/" },
+        // {icon: "⚙️", text: "Settings", link:"/" },
     ]
 
     return(
-        <div>
-            <div>
-                <h2>App Name</h2>
+        <div className={`${isOpen ? 'w-60' : 'w-0'} overflow-hidden bg-gray-900 text-white transition-all duration-300`} >
+            <div className="p-4 ">
+                <span className="flex flex-col ">
+                    <h2 className="text-center font-bold text-xl">ARK</h2>
+                    <h3 className="text-center font-bold ">Agriventory</h3>
+                </span>
             </div>
-            <nav>
-                <ul>
-                    {
-                        menuItems.map((item, index) =>(<li key={index}><i>{item.icon}</i>{item.text}</li>))
-                    }
-                </ul>
+            <nav className="mt-6 ">
+              
+                {
+                    menuItems.map((item, index) =>(
+                        <Link 
+                            key={index}
+                            to={item.link}
+                            className={`flex items-center px-6 py- 3 transition-colors 
+                                ${location.pathname == item.link ? 'bg-gray-800 border-l-4 border-blue-500' 
+                                    : 'hover: bg-gray-800'}`}
+                        >
+                            <span className="text-xl mr-3">{item.icon}</span>
+                            {item.text}
+                        </Link>
+                    ))
+                }
+                
             </nav>
-            <h1>{status ? "Open" : "Close"}</h1>
         </div>
     )
 }
